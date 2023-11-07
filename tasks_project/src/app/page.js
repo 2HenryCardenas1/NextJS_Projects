1dff8f5e705a5262060f92f85b77704b37020f69
2HenryCardenas1 <cardenashenryesteban@gmail.com>
2023-11-07 17:54:08 -0500

    Add task list to home page

--- tasks_project/src/app/page.js
+++ tasks_project/src/app/page.js
@@ -1,8 +1,30 @@
-export default function Home() {
+import { GetTasks } from "@/api/task";
+import TaskCard from "@/components/TaskCard";
+
+async function getTasks() {
+  try {
+    const tasks = await GetTasks();
+    return tasks;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export default async function HomePage() {
+  const tasks = await getTasks();
+
   return (
-    <div>
-      <h1>Tasks App</h1>
-      <p>Project management made easy.</p>
+    <div className="text-center mt-20">
+      <h1 className="font-bold text-5xl">Tasks App</h1>
+      <p className="font-sans text-xl mt-1">Your tasks list.</p>
+      <section className="container mx-auto">
+        <div className="grid grid-cols-3 gap-5 mt-5">
+          {tasks.map((task) => {
+            return <TaskCard task={task} key={task.id} />;
+          })}
+        </div>
+      </section>
     </div>
   );
 }
