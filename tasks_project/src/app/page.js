8aa5d57fdd1d7ebeec79ffee4e1924fc6ca50c7d
2HenryCardenas1 <cardenashenryesteban@gmail.com>
2023-11-08 07:13:01 -0500

    Refactor task retrieval in HomePage component

--- tasks_project/src/app/page.js
+++ tasks_project/src/app/page.js
@@ -1,15 +1,19 @@
-import { GetTasks } from "@/api/task";
 import TaskCard from "@/components/TaskCard";
+import { prisma } from "@/lib/prisma";
+
+async function getTasks() {
+  return prisma.task.findMany();
+}
 
 export default async function HomePage() {
-  const tasks = await GetTasks();
+  const tasks = await getTasks();
 
   return (
     <div className="text-center mt-20">
       <h1 className="font-bold text-5xl">Tasks App</h1>
       <p className="font-sans text-xl mt-1">Your tasks list.</p>
       <section className="container mx-auto">
-        <div className="grid   sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-5 mx-5 ">
+        <div className="grid grid-cols-1 gap-5 mx-5 md:grid-cols-2 lg:grid-cols-3  mt-5">
           {tasks.map((task) => {
             return <TaskCard task={task} key={task.id} />;
           })}
