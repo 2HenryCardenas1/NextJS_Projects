3f833d377970a32ef6b962a272a933bb3ce3b4df
2HenryCardenas1 <cardenashenryesteban@gmail.com>
2023-11-08 07:12:43 -0500

    Add API request functions for task management

---
+++ tasks_project/src/api/task_req.js
@@ -0,0 +1,70 @@
+import { URL_API } from "@/utils/constants";
+
+// File to manage the requests to the API
+
+export async function CreateTask(task) {
+  const res = await fetch(`${URL_API}/api/tasks`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(task),
+  });
+
+  const result = await res.json();
+
+  if (!res.ok) {
+    throw result;
+  }
+
+  return result;
+}
+
+export async function GetTasks() {
+  const rest = await fetch(`${URL_API}/api/tasks/`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const result = await rest.json();
+
+  if (!rest.ok) {
+    throw result;
+  }
+
+  return result;
+}
+
+export async function GetTask(id) {
+  const rest = await fetch(`${URL_API}/api/tasks/${id}`);
+
+  const result = await rest.json();
+
+  if (!rest.ok) {
+    throw result;
+  }
+
+  return result;
+}
+
+export async function UpdateTask(id, task) {
+  try {
+    const rest = await fetch(`${URL_API}/api/tasks/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(task),
+    });
+    const result = await rest.json();
+    if (!rest.ok) {
+      throw result;
+    }
+    return result;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
