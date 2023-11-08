5ba90bc153d99f254f1a5002a37c6d57b697c057
2HenryCardenas1 <cardenashenryesteban@gmail.com>
2023-11-07 20:26:34 -0500

    Refactor code to use GetTasks function instead of getTasks

--- tasks_project/src/app/page.js
+++ tasks_project/src/app/page.js
@@ -1,25 +1,15 @@
 import { GetTasks } from "@/api/task";
 import TaskCard from "@/components/TaskCard";
 
-async function getTasks() {
-  try {
-    const tasks = await GetTasks();
-    return tasks;
-  } catch (error) {
-    console.log(error);
-    throw error;
-  }
-}
-
 export default async function HomePage() {
-  const tasks = await getTasks();
+  const tasks = await GetTasks();
 
   return (
     <div className="text-center mt-20">
       <h1 className="font-bold text-5xl">Tasks App</h1>
       <p className="font-sans text-xl mt-1">Your tasks list.</p>
       <section className="container mx-auto">
-        <div className="grid grid-cols-3 gap-5 mt-5">
+        <div className="grid   sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-5 mx-5 ">
           {tasks.map((task) => {
             return <TaskCard task={task} key={task.id} />;
           })}
