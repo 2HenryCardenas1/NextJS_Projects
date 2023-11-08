ccc8186989eb2cc0924cef562d25a2707531b855
2HenryCardenas1 <cardenashenryesteban@gmail.com>
2023-11-08 08:58:17 -0500

    Add task status toggle and fix UI layout in TaskCard component

--- tasks_project/src/components/TaskCard.jsx
+++ tasks_project/src/components/TaskCard.jsx
@@ -1,32 +1,51 @@
 "use client";
 
+import { UpdateTask } from "@/api/task_req";
 import { useRouter } from "next/navigation";
+import { useCallback, useState } from "react";
 
 export default function TaskCard({ task }) {
   const router = useRouter();
 
-  return (
-    <div
-      className="bg-slate-800 p-8 rounded-lg hover:bg-slate-700 hover:cursor-pointer flex flex-col gap-5 h-96"
-      onClick={() => router.push(`/tasks/edit/${task.id}`)}
-    >
-      <div>
-        <div className="text-right">
-          <span className="text-sm text-gray-400 ">
-            {task.completed ? "Completed" : "Pending"}
-          </span>
-          <span
-            className={`inline-block ml-2 px-2 py-1 rounded-full ${
-              task.completed ? "bg-green-500" : "bg-red-500"
-            }`}
-          />
-        </div>
+  const lenghtDesc = task.description.length;
 
-        <h2 className="font-bold text-3xl text-center">{task.title}</h2>
-        <p className="text-white break-words">{task.description}</p>
+  const [status, setStatus] = useState(task.completed);
+
+  const handleStatus = useCallback(async () => {
+    await UpdateTask(task.id, {
+      completed: !status,
+    });
+    setStatus(!status);
+    router.refresh();
+  }, [status, task, router]);
+
+  return (
+    <div className="bg-slate-800 p-8 rounded-lg hover:bg-slate-700 hover:cursor-pointer flex flex-col gap-5 h-[30rem] ">
+      <div className="text-right hover:cursor-pointer " onClick={handleStatus}>
+        <span className="text-sm text-gray-400 ">
+          {status ? "Completed" : "Pending"}
+        </span>
+        <span
+          className={`inline-block ml-2 px-2 py-1 rounded-full ${
+            status ? "bg-green-500" : "bg-red-500"
+          }`}
+        />
+      </div>
+      <div
+        className="flex-1 flex flex-col "
+        onClick={() => router.push(`/tasks/edit/${task.id}`)}
+      >
+        <h2 className="font-bold text-3xl text-center mb-2">{task.title}</h2>
+        <div
+          className={`overflow-auto max-h-[250px] flex flex-col  items-center flex-1 ${
+            lenghtDesc < 250 && "justify-center"
+          }`}
+        >
+          <p className="text-white break-words  ">{task.description}</p>
+        </div>
       </div>
 
-      <div className="flex  justify-between flex-1 items-end">
+      <div className="flex  justify-between  items-end">
         <p className="text-gray-400 align-bottom flex flex-col">
           <span className="text-sm">Created</span>
           {new Date(task.createdAt).toLocaleDateString()}
